5f10985930ac2f13c765b79030c30baf7ee39dde
dlukin <dlukin@phoenixit.ru>
2021-12-13 12:05:00 +0300

    refactor for new datafiles

--- crm-bh/src/main/java/ru/filit/mdma/crm/Application.java
+++ crm-bh/src/main/java/ru/filit/mdma/crm/Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Запуск приложения CRM Business Hub.
  */
 @SpringBootApplication
-public class CrmBhApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(CrmBhApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
--- dm/src/main/java/ru/filit/mdma/dm/Application.java
+++ dm/src/main/java/ru/filit/mdma/dm/Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Запуск приложения DataManager.
  */
 @SpringBootApplication
-public class DataManagerApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(DataManagerApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
